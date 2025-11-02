71951eb9820cb7aed8ea7f9442576ed391e5de4e
Tamilselvanpalani1 <tamilselvanpalani1@gmail.com>
2025-11-02 19:58:44 +0530

    olapal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-base-200" data-theme="dark">
-      <h1 className="text-3xl font-bold mb-6">Welcome to the Twitter Clone</h1>
+      <h1 className="text-3xl font-bold mb-6 btn">Welcome to the Twitter Clone</h1>
     </div>
   );
 }
